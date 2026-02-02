6c6fdf94d01e3fee22520aee61c6bd3c973173b2
wldnjs1111 <116607302+wldnjs1111@users.noreply.github.com>
2026-02-02 21:11:25 +0900

    Time: 2 ms (94.25%), Space: 72.3 MB (24.41%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+
+        output[0] = 1;
+        for(int i=1; i<n; i++){
+            output[i] = output[i-1] * nums[i-1]; 
+        }
+
+        int postfix = 1;
+        for(int i=n-1;i>=0;i--){
+            output[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return output;
+    }
+}
